333e0364b9e13a4e6ca6d5c1e77b28ec855005f5
dognoir <don.nichol@gmail.com>
2013-10-21 16:54:11 -0700

    forgot the my project folder

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Alarm")> 
+<Assembly: AssemblyDescription("A repeating alarm that prompts you to get off the computer, walk around and clear your head")> 
+<Assembly: AssemblyCompany("Don Nichol")> 
+<Assembly: AssemblyProduct("Alarm")> 
+<Assembly: AssemblyCopyright("Copyright © Don Nichol 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("05b43517-2877-43fc-b8de-6aa122ded866")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.0.0.0")> 
+<Assembly: AssemblyFileVersion("2.0.0.0")> 
